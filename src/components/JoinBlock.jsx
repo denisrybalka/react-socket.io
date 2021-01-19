bea3fcb9ecd66e739749f977cf8ef402cc1dc6f9
Denis Rybalka <denis.rubalka1@gmail.com>
2021-01-19 21:50:02 +0200

    Split client & server

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,58 +1,62 @@
-import React, { useState } from 'react'
-import socket from '../socket'
-import axios from 'axios'
+import React, { useState } from "react";
+import socket from "../socket";
+import axios from "axios";
 
 const JoinBlock = ({ onLogin }) => {
+  const [input, setInput] = useState({
+    roomId: "",
+    username: "",
+  });
 
-    const [input, setInput] = useState({
-        roomId: "",
-        username: "",
-    })
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  const handleInputChange = (e) => {
+    setInput((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
-    const handleInputChange = (e) => {
-        setInput((prev) => {
-            return {
-                ...prev,
-                [e.target.name]: e.target.value,
-            }
-        })
+  const onEnter = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const { roomId, username } = input;
+    if (!roomId || !username) {
+      setLoading(false);
+      return alert("Неверные данные!");
     }
 
-    const onEnter = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        const { roomId, username } = input;
-        if (!roomId || !username) {
-            setLoading(false);
-            return alert("Неверные данные!");
-        }
+    const user = { roomId, username };
+    await axios
+      .post("/rooms", user)
+      .then(() => onLogin(user))
+      .catch((e) => {
+        setLoading(false);
+        alert(e.response.data.message);
+      });
+  };
 
-        const user = { roomId, username};
-        await axios.post("/rooms", user);
-        onLogin(user);
-    }
-
-    return (
-        <form className="join-form" onSubmit={onEnter}>
-            <input
-                type="text"
-                placeholder="Room ID"
-                name="roomId"
-                value={input.roomId}
-                onChange={handleInputChange}
-            />
-            <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={input.username}
-                onChange={handleInputChange}
-            />
-            <button disabled={loading}>{ loading ? "ВХОД..." : "ВОЙТИ"}</button>
-        </form>
-    )
-}
+  return (
+    <form className="join-form" onSubmit={onEnter}>
+      <input
+        type="text"
+        placeholder="Room ID"
+        name="roomId"
+        value={input.roomId}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        value={input.username}
+        onChange={handleInputChange}
+      />
+      <button disabled={loading}>{loading ? "ВХОД..." : "ВОЙТИ"}</button>
+    </form>
+  );
+};
 
 export default JoinBlock;
